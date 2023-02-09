9b1ce8eed262a54cf45a3769d6570e31bd3639c9
Nathan Drake <nathandrakedrums@gmail.com>
2023-02-08 20:12:18 -0600

    chainge engines

---
+++ hooks/useChain.ts
@@ -0,0 +1,18 @@
+export enum Chain {
+  mainnet = 'mainnet',
+  goerli = 'goerli',
+}
+
+export const useChain = () => {
+  const chainId = Chain.goerli;
+
+  const rpc = {
+    [Chain.mainnet]: process.env.NEXT_PUBLIC_RPC_URL_1 as string,
+    [Chain.goerli]: process.env.NEXT_PUBLIC_RPC_URL_5 as string,
+  };
+
+  return {
+    chainId,
+    rpc: rpc[chainId],
+  };
+};
