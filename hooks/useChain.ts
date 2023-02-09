8c0574b633fa4fc367ea5c7da059b72cd2f3790b
Nathan Drake <nathandrakedrums@gmail.com>
2023-02-09 09:17:38 -0600

    refactor: Use env var for chain

--- hooks/useChain.ts
+++ hooks/useChain.ts
@@ -1,7 +1,13 @@
 import { Chain } from 'web3/types';
 
 export const useChain = () => {
-  const chainId = Chain.goerli;
+  const envChain = process.env.NEXT_PUBLIC_CHAIN;
+
+  if (!envChain) {
+    throw new Error('NEXT_PUBLIC_CHAIN is not defined');
+  }
+
+  const chainId = envChain === 'goerli' ? Chain.goerli : Chain.mainnet;
 
   const rpc = {
     [Chain.mainnet]: process.env.NEXT_PUBLIC_RPC_URL_1 as string,
